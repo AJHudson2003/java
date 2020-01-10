e0bf79d1b0b6d54968bedffa9c0db7fe9110620d
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2020-01-10 09:39:43 -0500

    added comment and submit order code
    tested on nexus 5

--- app/src/main/java/com/example/java/MainActivity.java
+++ app/src/main/java/com/example/java/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.java;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+
+     This method is called when the order is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+    /**
+
+     This method displays the given quantity value on screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
