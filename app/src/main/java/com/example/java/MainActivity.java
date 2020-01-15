3737e844b76a7708c821cdf9d017981bae400a68
AJ Hudson <02andrewjames.hudson@gulllakecs.org>
2020-01-15 09:30:06 -0500

    added price and math count
    tested on nexus 5

--- app/src/main/java/com/example/java/MainActivity.java
+++ app/src/main/java/com/example/java/MainActivity.java
@@ -1,31 +1,50 @@
 package com.example.java;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+
+This app displays an order form to order coffee
+*/
 public class MainActivity extends AppCompatActivity {
+int numofcoffes = 0;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+/**
 
-    /**
+This method is called when the order is clicked.
+*/
+public void submitOrder(View view) {
 
-     This method is called when the order is clicked.
-     */
-    public void submitOrder(View view) {
-        display(16 - 10);
-    }
-    /**
+numofcoffes++;
+display(numofcoffes);
+displayPrice(numofcoffes * 5);
+}
+/**
 
-     This method displays the given quantity value on screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = findViewById(R.id.zero_text_view);
-        quantityTextView.setText("" + number);
-    }
+This method displays the given quantity value on screen.
+*/
+private void display(int number) {
+TextView quantityTextView = findViewById(R.id.quantity_text_view);
+quantityTextView.setText("" + number);
+}
+/**
+
+This method displays the given price on the screen.
+*/
+private void displayPrice(int number) {
+TextView priceTextView = findViewById(R.id.zero_text_view);
+priceTextView.setText(NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(number));
+}
 }
